88016d49ac2ed95b8b4152463e329b8602e7204e
3000001 <sereeviriyakul.k@gmail.com>
2026-09-06 19:56:24 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -6,17 +6,13 @@ app = Flask(__name__)
 def webhook():
     data = request.get_json()
 
-    print("ข้อมูลจาก LINE:")
-    print(data)
-
+    print("FULL EVENT", data, flush=True)
     events = data.get("events", [])
 
     for event in events:
         source = event.get("source", {})
         user_id = source.get("userId")
-
-        if user_id:
-            print("USER ID ของปาป้า =", user_id)
+        print ("USER ID:", user_id)
 
     return "OK", 200
 
